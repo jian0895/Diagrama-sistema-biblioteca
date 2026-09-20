5f4d9767f116229d5d4a13bfcc5029f0b8cb353b
Cristian Gomez <gomezcristianbad@gmail.com>
2026-09-20 12:18:02 -0500

    corrige clase Libro

--- clases_del_sistema_de_bibliotecas/src/biblioteca/Libro.java
+++ clases_del_sistema_de_bibliotecas/src/biblioteca/Libro.java
@@ -1,26 +1,69 @@
-
-package co.edu.iudigital.sistema;
-
 public class Libro {
-    private String titulo;
-    private String autor;
-    private String genero;
-    private boolean disponible;
 
-    public Libro(String titulo, String autor, String genero) {
-        this.titulo = titulo;
-        this.autor = autor;
-        this.genero = genero;
-        this.disponible = true;
+    // 1. Atributos
+    protected String idLibro;
+    protected String titulo;
+    protected Autor autor;
+    protected boolean disponible;
+    protected String generoLiterario;
+
+    // 2. Constructores
+    public Libro() {
     }
 
-    public String getTitulo() { return titulo; }
-    public String getAutor() { return autor; }
-    public boolean isDisponible() { return disponible; }
-    public void setDisponible(boolean disponible) { this.disponible = disponible; }
+    public Libro(String idLibro, String titulo, Autor autor,
+                 boolean disponible, String generoLiterario) {
 
-    @Override
-    public String toString() {
-        return "Libro: " + titulo + " | Autor: " + autor + " | Estado: " + (disponible ? "Disponible" : "Prestado");
+        this.idLibro = idLibro;
+        this.titulo = titulo;
+        this.autor = autor;
+        this.disponible = disponible;
+        this.generoLiterario = generoLiterario;
+    }
+
+    // 3. Getters y Setters
+    public String getIdLibro() {
+        return idLibro;
+    }
+
+    public void setIdLibro(String idLibro) {
+        this.idLibro = idLibro;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public Autor getAutor() {
+        return autor;
+    }
+
+    public void setAutor(Autor autor) {
+        this.autor = autor;
+    }
+
+    public boolean isDisponible() {
+        return disponible;
+    }
+
+    public void setDisponible(boolean disponible) {
+        this.disponible = disponible;
+    }
+
+    public String getGeneroLiterario() {
+        return generoLiterario;
+    }
+
+    public void setGeneroLiterario(String generoLiterario) {
+        this.generoLiterario = generoLiterario;
+    }
+
+    // 4. Método del UML
+    public String obtenerInstruccionesAcceso() {
+        return "Consultar disponibilidad en la biblioteca.";
     }
 }
